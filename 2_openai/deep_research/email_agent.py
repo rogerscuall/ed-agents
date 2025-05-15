9469a42c2245ab02892b0b54fe6ddc341ff84e5a
Roger Gomez <rogerscuall@gmail.com>
2025-05-15 14:37:49 -0400

    comment to save time.

--- 2_openai/deep_research/email_agent.py
+++ 2_openai/deep_research/email_agent.py
@@ -8,13 +8,13 @@ from agents import Agent, function_tool
 @function_tool
 def send_email(subject: str, html_body: str) -> Dict[str, str]:
     """ Send an email with the given subject and HTML body """
-    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
-    from_email = Email("website.stray_1y@icloud.com") # put your verified sender here
-    to_email = To("kenitaglez@yahoo.es") # put your recipient here
-    content = Content("text/html", html_body)
-    mail = Mail(from_email, to_email, subject, content).get()
-    response = sg.client.mail.send.post(request_body=mail)
-    print("Email response", response.status_code)
+    # sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+    # from_email = Email("website.stray_1y@icloud.com") # put your verified sender here
+    # to_email = To("kenitaglez@yahoo.es") # put your recipient here
+    # content = Content("text/html", html_body)
+    # mail = Mail(from_email, to_email, subject, content).get()
+    # response = sg.client.mail.send.post(request_body=mail)
+    # print("Email response", response.status_code)
     return {"status": "success"}
 
 INSTRUCTIONS = """You are able to send a nicely formatted HTML email based on a detailed report.
