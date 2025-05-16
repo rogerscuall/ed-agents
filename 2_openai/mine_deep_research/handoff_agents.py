3ee5215df7bd331578a236f95a36e3b2c9d180ad
Roger Gomez <rogerscuall@gmail.com>
2025-05-16 12:35:24 -0400

    Add email handoff functionality to integrate email agent

--- 2_openai/mine_deep_research/handoff_agents.py
+++ 2_openai/mine_deep_research/handoff_agents.py
@@ -1,14 +1,16 @@
 from agents import Agent, handoff, RunContextWrapper
 from email_agent import email_agent
 from agents.extensions import handoff_filters
+from writer_agent import ReportData
 
-def email_handoff(ctx: RunContextWrapper[None]):
+async def email_handoff(ctx: RunContextWrapper[None], input_data: ReportData):
     yield f"Handing off to email agent..."
 
 email_handoff_agent = handoff(
     agent=email_agent,
     on_handoff=email_handoff,
     tool_name_override="email_agent",
-    tool_description="Send an email with a report.",
-    input_filter=handoff_filters.remove_all_tools,
+    tool_description_override="Send an email with a report.",
+    input_type=ReportData,
+    
 )
